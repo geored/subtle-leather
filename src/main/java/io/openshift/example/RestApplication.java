64f3e44d5774baa12399e1017e8ae00215eeb61f
Red Hat Developers Launcher <45641108+redhat-developers-launcher@users.noreply.github.com>
2021-05-15 11:02:59 +0000

    Initial commit

---
+++ src/main/java/io/openshift/example/RestApplication.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.openshift.example;
+
+import io.openshift.example.service.Greeting;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.auth.PubSecKeyOptions;
+import io.vertx.ext.auth.jwt.JWTAuth;
+import io.vertx.ext.auth.jwt.JWTAuthOptions;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.JWTAuthHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+
+import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
+
+public class RestApplication extends AbstractVerticle {
+
+  private final Logger log = LoggerFactory.getLogger(RestApplication.class);
+  private long counter;
+
+  @Override
+  public void start(Promise<Void> done) {
+    // Create a router object.
+    Router router = Router.router(vertx);
+    router.get("/health").handler(rc -> rc.response().end("OK"));
+
+    JsonObject keycloakJson = new JsonObject()
+      .put("realm", System.getenv("REALM"))
+      .put("auth-server-url", System.getenv("SSO_AUTH_SERVER_URL"))
+      .put("ssl-required", "external")
+      .put("resource", System.getenv("CLIENT_ID"))
+      .put("credentials", new JsonObject()
+        .put("secret", System.getenv("SECRET")));
+
+    // Configure the AuthHandler to process JWT's
+    router.route("/greeting").handler(JWTAuthHandler.create(
+      JWTAuth.create(vertx, new JWTAuthOptions()
+        .addPubSecKey(new PubSecKeyOptions()
+          .setAlgorithm("RS256")
+          .setPublicKey(System.getenv("REALM_PUBLIC_KEY")))
+        // since we're consuming keycloak JWTs we need to locate the permission claims in the token
+        .setPermissionsClaimKey("realm_access/roles"))));
+
+    // This is how one can do RBAC, e.g.: only admin is allowed
+    router.get("/greeting").handler(ctx ->
+      ctx.user().isAuthorized("booster-admin", authz -> {
+        if (authz.succeeded() && authz.result()) {
+          ctx.next();
+        } else {
+          log.error("AuthZ failed!");
+          ctx.fail(403);
+        }
+      }));
+
+    router.get("/greeting").handler(ctx -> {
+      String name = ctx.request().getParam("name");
+      if (name == null) {
+        name = "World";
+      }
+      ctx.response()
+        .putHeader(CONTENT_TYPE, "application/json; charset=utf-8")
+        .end(new Greeting(++counter, name).encode());
+    });
+
+    // serve the dynamic config so the web client
+    // can also connect to the SSO server
+    router.get("/keycloak.json").handler(ctx ->
+      ctx.response()
+        .putHeader(CONTENT_TYPE, "application/json; charset=utf-8")
+        .end(keycloakJson.encode()));
+
+    // serve static files (web client)
+    router.get().handler(StaticHandler.create());
+
+    // Create the HTTP server and pass the "accept" method to the request handler.
+    vertx
+      .createHttpServer()
+      .requestHandler(router)
+      .listen(
+        // Retrieve the port from the configuration,
+        // default to 8080.
+        config().getInteger("http.port", 8080),
+        ar -> done.handle(ar.mapEmpty()));
+  }
+}
---
+++ src/test/java/io/openshift/example/SecuredBoosterIT.java
@@ -0,0 +1,142 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.openshift.example;
+
+import org.arquillian.cube.kubernetes.impl.utils.CommandExecutor;
+import org.arquillian.cube.openshift.impl.enricher.AwaitRoute;
+import org.arquillian.cube.openshift.impl.enricher.RouteURL;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.*;
+import org.junit.runner.RunWith;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.*;
+
+@RunWith(Arquillian.class)
+public class SecuredBoosterIT {
+
+  @RouteURL("${app.name}")
+  @AwaitRoute
+  private URL exampleEndpoint;
+
+  private static final CommandExecutor COMMAND_EXECUTOR = new CommandExecutor();
+  private static String ssoEndpoint;
+
+  @BeforeClass
+  public static void init() {
+    // You can disable the sso server initialization by setting the system property skip.sso.init to true
+    if (!Boolean.getBoolean("skip.sso.init")) {
+      // Remove service account
+
+      try {COMMAND_EXECUTOR.execCommand("oc delete sa sso-service-account");}
+      catch(Exception ignore) {}
+      
+      COMMAND_EXECUTOR.execCommand("oc create -f service.sso.yaml");
+    }
+    ssoEndpoint = COMMAND_EXECUTOR
+      .execCommand("oc get route secure-sso -o jsonpath='{\"https://\"}{.spec.host}{\"/auth\"}'")
+      .get(0)
+      .replace("'", ""); // for some reason, the string contains a single ' before the URL
+
+    /* Await the sso server to be ready so we can make token requests. We cannot use @AwaitRoute as we are deploying
+    the sso server as part of the tests.
+    When making requests too early (the sso server is not ready yet), it throws SSLHandshakeException,
+    so we are taking care of that in the try-catch here. */
+    await().atMost(5, TimeUnit.MINUTES).until(() -> {
+      try {
+        return given().relaxedHTTPSValidation().when().get(ssoEndpoint).statusCode() == 200;
+      } catch (Exception ex) {
+        return false;
+      }
+    });
+  }
+
+  @Test
+  public void defaultUser_defaultFrom() {
+    String token = getToken("alice", "password");
+
+    given().header("Authorization", "Bearer " + token)
+      .when().get(exampleEndpoint.toString() + "greeting")
+      .then().body("content", equalTo("Hello, World!"));
+  }
+
+  @Test
+  public void defaultUser_customFrom() {
+    String token = getToken("alice", "password");
+
+    given().header("Authorization", "Bearer " + token)
+      .when().get(exampleEndpoint.toString() + "greeting?name=Scott")
+      .then().body("content", equalTo("Hello, Scott!"));
+  }
+
+  @Test
+  public void adminUser() {
+    String token = getToken("admin", "admin");
+
+    given().header("Authorization", "Bearer " + token)
+      .when().get(exampleEndpoint.toString() + "greeting")
+      .then().statusCode(403); // should be 403 Forbidden, as the admin user does not have the required role
+  }
+
+  @Test
+  public void badPassword() {
+    String token = getToken("alice", "bad");
+
+    given().header("Authorization", "Bearer " + token)
+      .when().get(exampleEndpoint.toString() + "greeting?name=Scott")
+      .then().statusCode(401); // should be 401 Unauthorized, as auth fails because of providing bad password (token is null)
+  }
+
+  // SSO server cleanup
+  @AfterClass
+  public static void deleteSSO() {
+    if (!Boolean.getBoolean("skip.sso.init")) {
+      COMMAND_EXECUTOR.execCommand("oc delete all --selector application=sso");
+      COMMAND_EXECUTOR.execCommand("oc delete secret sso-app-secret");
+      COMMAND_EXECUTOR.execCommand("oc delete secret sso-demo-secret");
+      COMMAND_EXECUTOR.execCommand("oc delete serviceaccount sso-service-account");
+    }
+  }
+
+  private String getToken(String username, String password) {
+    Map<String, String> requestParams = new HashMap<>();
+    try {
+      requestParams.put("grant_type", "password");
+      requestParams.put("username", URLEncoder.encode(username, "UTF8"));
+      requestParams.put("password", URLEncoder.encode(password, "UTF8"));
+      requestParams.put("client_id", "demoapp");
+      requestParams.put("client_secret", "1daa57a2-b60e-468b-a3ac-25bd2dc2eadc");
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+    }
+
+    return given()
+      .relaxedHTTPSValidation()
+      .params(requestParams)
+      .when()
+      .post(ssoEndpoint + "/realms/master/protocol/openid-connect/token")
+      .path("access_token");
+  }
+}
